c5d4a80e5700a14f28308c23dfc936b5c04ead7e
coco <kokotenrico@gmail.com>
2022-03-17 09:27:25 +0100

    Implement Axios

---
+++ src/components/Commit.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    {{ details.sha }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["details"],
+  name: "Commit",
+};
+</script>
--- src/views/Commits.vue
+++ src/views/Commits.vue
@@ -1,10 +1,29 @@
 <template>
   <h1>This is the Home page</h1>
-  <router-link to="details">Details</router-link>
+  <router-link to="details">
+    <Commit v-for="commit in commits" :key="commit.node_id" :details="commit" />
+  </router-link>
 </template>
 
 <script>
+import axios from "axios";
+import Commit from "@/components/Commit.vue";
+
 export default {
   name: "Commits",
+  data: () => ({
+    commits: [],
+  }),
+  created() {
+    axios
+      .get("https://api.github.com/repos/vuejs/vue/commits")
+      .then((response) => {
+        // console.log(response.data);
+        this.commits = response.data;
+      });
+  },
+  components: {
+    Commit,
+  },
 };
 </script>
